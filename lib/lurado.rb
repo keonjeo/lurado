afb74cf4791f357cf6b9948133acd671e5eabc23
Keon Ye <jnuyeyizhou@gmail.com>
2018-09-18 15:13:19 +0800

    add some rdoc comment into the lurado.rb file

--- lib/lurado.rb
+++ lib/lurado.rb
@@ -1,4 +1,14 @@
 class Lurado
+
+  # Say hi to the world!
+  #
+  # Example:
+  #   >> Lurado.hi("spanish")
+  #   => hola mundo
+  #
+  # Arguments:
+  #   language: (String)
+
   def self.hi(language = "english")
     translator = Translator.new(language)
     translator.hi
